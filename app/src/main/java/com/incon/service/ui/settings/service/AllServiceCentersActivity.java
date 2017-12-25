f1d47b944321ed595961ddacd3d40f989ac5d551
shivakoka <koka.shiva@gmail.com>
2017-12-25 15:30:20 +0530

    1. Added dialog for new service centers

--- app/src/main/java/com/incon/service/ui/settings/service/AllServiceCentersActivity.java
+++ app/src/main/java/com/incon/service/ui/settings/service/AllServiceCentersActivity.java
@@ -1,5 +1,6 @@
 package com.incon.service.ui.settings.service;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
@@ -18,12 +19,17 @@ import android.widget.TextView;
 import com.incon.service.R;
 import com.incon.service.apimodel.components.servicecenterresponse.ServiceCenterResponse;
 import com.incon.service.callbacks.IClickCallback;
+import com.incon.service.custom.view.AppListDialog;
 import com.incon.service.custom.view.CustomTextInputLayout;
+import com.incon.service.custom.view.PickImageDialog;
 import com.incon.service.databinding.ActivityAddDesignationsBinding;
 import com.incon.service.databinding.ActivityAllServiceCentersBinding;
 import com.incon.service.dto.adddesignation.AddDesignation;
 import com.incon.service.ui.BaseActivity;
+import com.incon.service.ui.adddesignations.AddDesignationsActivity;
 import com.incon.service.ui.adddesignations.AddDesignationsPresenter;
+import com.incon.service.ui.addservicecenter.AddServiceCenterActivity;
+import com.incon.service.ui.adduser.AddUserActivity;
 import com.incon.service.ui.settings.adapters.AllServiceCentersAdapter;
 import com.incon.service.utils.SharedPrefsUtils;
 import com.weiwangcn.betterspinner.library.material.MaterialBetterSpinner;
@@ -44,6 +50,7 @@ public class AllServiceCentersActivity extends BaseActivity implements
 
     public List<ServiceCenterResponse> serviceCenterResponseList;
     private AllServiceCentersAdapter allServiceCentersAdapter;
+    private AppListDialog appListDialog;
 
     @Override
     protected int getLayoutId() {
@@ -97,13 +104,54 @@ public class AllServiceCentersActivity extends BaseActivity implements
                 finish();
             }
         });
+        binding.toolbarNewIv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNewOptionDialog();
+            }
+        });
     }
 
+    private void showNewOptionDialog() {
+        String[] items = getResources().getStringArray(R.array.select_new_options);
+
+        appListDialog = new AppListDialog();
+        appListDialog.setClickCallback(clickCallback);
+        appListDialog.initDialogLayout(this, items);
+    }
+
+    IClickCallback clickCallback = new IClickCallback() {
+        @Override
+        public void onClickPosition(int position) {
+            Intent intent = null;
+            switch (position) {
+                case 0:
+                    //load add new center screen;
+                    intent = new Intent(AllServiceCentersActivity.this, AddServiceCenterActivity.class);
+                    break;
+                case 1:
+                    //load new user screen
+                    intent = new Intent(AllServiceCentersActivity.this, AddUserActivity.class);
+                    break;
+                case 2:
+                    //load designations screen
+                    intent = new Intent(AllServiceCentersActivity.this, AddDesignationsActivity.class);
+                    break;
+                default:
+                    break;
+            }
+            if (intent != null) {
+                startActivity(intent);
+            }
+
+        }
+    };
 
     /**
      * onclick listener for add designations
      */
     public void onClickNewDesignation() {
+
 
     }
 
